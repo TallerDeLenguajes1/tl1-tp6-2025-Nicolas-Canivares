ebb75c045d6154116d16750a2718d7cc9e255d7b
Guzman Canivares Enzo Nicolas <nicocanivares2002@gmail.com>
2025-05-21 00:01:30 -0300

    Ejercicio1 Numero Invertido

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -1,9 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Ejercicio1
-Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+//Ejercicio1 Invertir Numero
+Console.WriteLine("Ingrese un numero: ");
+var numeroIngresado = Console.ReadLine();
+int numero;
+bool esNumero = int.TryParse(numeroIngresado, out numero);
+int resto, numeroInvertido = 0;
+
+if (esNumero)
+{
+    Console.WriteLine("Numero Ingresado: " + numero);
+    while (numero > 0)
+    {
+        resto = numero % 10;
+        numeroInvertido = (numeroInvertido * 10) + resto;
+        numero /= 10;
+    }
+}
+
+Console.WriteLine("Numero invertido: " + numeroInvertido);
