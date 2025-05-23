5672b8dec95d4ad4ffa888299094cde2d238a5d0
Guzman Canivares Enzo Nicolas <nicocanivares2002@gmail.com>
2025-05-23 18:36:35 -0300

    CalculadoraV1

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -26,3 +26,59 @@ else
 
 //Ejercicio02 Calculadora
 
+int opcion = 0;
+do
+{
+    Console.WriteLine("Que operacion desea realizar?");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Salir");
+    Console.WriteLine("Ingrese un numero: ");
+    var operacion = Console.ReadLine();
+    int numero1 = 0, numero2 = 0, resultado = 0;
+    bool operacionElegida = int.TryParse(operacion, out opcion);
+    if (opcion >= 1 && opcion < 5)
+    {
+        Console.WriteLine("Ingrese el primer numero");
+        int.TryParse(Console.ReadLine(), out numero1);
+        //numero1 = Console.Read();
+        Console.WriteLine("Ingrese el segundo numero");
+        int.TryParse(Console.ReadLine(), out numero2);
+        //numero2 = Console.Read();
+    }
+
+
+    switch (opcion)
+    {
+        case 1:
+            //Suma
+            resultado = numero1 + numero2;
+            break;
+        case 2:
+            //Resta
+            resultado = numero1 - numero2;
+            break;
+        case 3:
+            //Multiplicacion
+            resultado = numero1 * numero2;
+            break;
+        case 4:
+            //Division
+            resultado = numero1 / numero2;
+            break;
+        case 5:
+            Console.WriteLine("Saliendo...");
+            break;
+        default:
+            Console.WriteLine("No ingresó una opción valida");
+            break;
+    }
+
+    if (numero1 != 0 || numero2 != 0)
+    {
+        Console.WriteLine("El resultado es: " + resultado);
+    }
+
+} while (opcion != 5);
